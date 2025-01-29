dacd4f0548b460ab050e4836fd009106a0abcdf7
muslimbeko8 <blakmaxmudov@gmail.com>
2025-01-29 23:21:46 +0500

    fdfg

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
-'use client';
-import { useEffect } from 'react';
-
 const Home = () => {
-  useEffect(() => {
-    const tg = window.Telegram.WebApp;
-    tg.expand(); // Ekranni kengaytirish (full screen)
-  }, []);
-
   return (
     <div>
       <h1>Salom, bu Telegram Web App!</h1>
